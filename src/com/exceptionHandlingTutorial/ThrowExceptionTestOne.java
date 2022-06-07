6fa740b00f33cfcadb3c0ff36891d8fb053698ac
Dragon2623 <armin1.aliani1@gmail.com>
2022-06-07 20:38:48 +0430

    add ThrowExceptionTestOne

---
+++ src/com/exceptionHandlingTutorial/ThrowExceptionTestOne.java
@@ -0,0 +1,22 @@
+package com.exceptionHandlingTutorial;
+
+public class ThrowExceptionTestOne {
+    public static void main(String[] args) {
+        System.out.println(divide(4, 2));
+        System.out.println(divide(5, 1));
+        System.out.println(divide(6, 0));
+    }
+
+    static int divide(int numberToDivide, int numberToDivideBy) {
+        if (numberToDivideBy == 0) {
+            throw new BadNumberException();
+            /*RuntimeException runtimeException = new RuntimeException("Cannot divide by 0");
+            throw runtimeException;*/
+//            throw new RuntimeException("Cannot divide by 0");
+        }
+        return numberToDivide / numberToDivideBy;
+    }
+}
+
+class BadNumberException extends RuntimeException {
+}

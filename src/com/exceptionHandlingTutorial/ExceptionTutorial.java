6996982e786e7a548d2304d197d015d833099660
Dragon2623 <armin1.aliani1@gmail.com>
2022-06-07 15:59:52 +0430

    add first exception

---
+++ src/com/exceptionHandlingTutorial/ExceptionTutorial.java
@@ -0,0 +1,27 @@
+package com.exceptionHandlingTutorial;
+
+public class ExceptionTutorial {
+    // When we put the first exception in a method, in the console,
+    // first the exception of that method is shown to us and then wherever that method calls the exception,
+    // it is displayed to us in order and note that Only the first Exceptions are displayed
+    public static void main(String[] args) {
+        A();
+    }
+
+    static void A() {
+        B();
+    }
+
+    static void B() {
+        C();
+        D();
+    }
+
+    static void C() {
+        throw new RuntimeException();
+    }
+
+    static void D() {
+        throw new RuntimeException();
+    }
+}
